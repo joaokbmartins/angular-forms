4675cd87b55e179653928c8f6633fbc2936c6a6e
Joao Martins <martins.joao1909@gmail.com>
2021-02-01 03:24:32 -0300

    Accessing values and reseting form. Template-Driven approach end.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,30 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+
+interface UserInfo {
+  username: string;
+  email: string;
+  gender: string;
+  secret: string;
+  secretAnswer: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   defaultQuestion: string = 'teacher';
   answer: string = null;
   genders: string[] = ['female', 'male'];
+  userinfo: UserInfo;
 
   @ViewChild('formRef') formRef: NgForm;
+
+  ngOnInit() {
+    this.userinfo = <UserInfo>{};
+  }
 
   onSuggestUsername() {
     const suggestedUsername: string = 'Supseruser';
@@ -34,5 +47,19 @@ export class AppComponent {
 
   onSubmit(form: NgForm) {
     console.log('Submitted!', form);
+    this.onSaveUserInfo();
+    this.onResetForm();
+  }
+
+  onSaveUserInfo() {
+    this.userinfo.username = this.formRef.value['user-data'].username;
+    this.userinfo.email = this.formRef.value['user-data'].mail;
+    this.userinfo.gender = this.formRef.value['user-data'].gender;
+    this.userinfo.secret = this.formRef.value.secret;
+    this.userinfo.secretAnswer = this.formRef.value.answer;
+  }
+
+  onResetForm() {
+    this.formRef.reset();
   }
 }
