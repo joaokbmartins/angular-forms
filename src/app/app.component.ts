a84ee8c2f3f60b827899d8e391e4d0bb82717e73
Joao Martins <martins.joao1909@gmail.com>
2021-02-01 03:03:02 -0300

    NgForm.set/patchValue()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -10,6 +10,27 @@ export class AppComponent {
   defaultQuestion: string = 'teacher';
   answer: string = null;
   genders: string[] = ['female', 'male'];
+
+  @ViewChild('formRef') formRef: NgForm;
+
+  onSuggestUsername() {
+    const suggestedUsername: string = 'Supseruser';
+    // this.formRef.setValue({
+    //   'user-data': {
+    //     username: suggestedUsername,
+    //     mail: '',
+    //     gender: '',
+    //   },
+    //   secret: 'teacher',
+    //   answer: '',
+    // });
+
+    this.formRef.form.patchValue({
+      'user-data': {
+        username: suggestedUsername,
+      },
+    });
+  }
 
   onSubmit(form: NgForm) {
     console.log('Submitted!', form);
