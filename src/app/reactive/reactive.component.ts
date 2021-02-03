d60434e9a38c1a140fb7ee142662b8693e4e08d2
Joao Martins <martins.joao1909@gmail.com>
2021-02-03 11:59:05 -0300

    Error messages - ReactiveForm

--- src/app/reactive/reactive.component.ts
+++ src/app/reactive/reactive.component.ts
@@ -7,15 +7,27 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./reactive.component.css'],
 })
 export class ReactiveComponent implements OnInit {
-  genders: string[] = ['female', 'male'];
-
   loginForm: FormGroup;
+  genders: string[] = ['female', 'male'];
+  forbiddenNames: string[];
+  errors: { [key: string]: string };
 
   ngOnInit() {
+    this.forbiddenNames = ['Tonny', 'Wayne'];
+    this.errors = {
+      required: 'This field is required',
+      forbiddenName: 'Username unavailable',
+      minLengthInvalid: 'Required length: 10 characters',
+      email: 'Invalid email',
+    };
     this.loginForm = new FormGroup({
       userData: new FormGroup({
-        username: new FormControl(null, Validators.required),
-        gender: new FormControl('male'),
+        username: new FormControl(null, [
+          Validators.required,
+          this.forbiddenNamesValidator.bind(this),
+          this.minLengthValidator.bind(this),
+        ]),
+        gender: new FormControl('male', [Validators.required]),
       }),
       email: new FormControl(null, [Validators.required, Validators.email]),
       hobbies: new FormArray([]),
@@ -24,6 +36,7 @@ export class ReactiveComponent implements OnInit {
 
   onSubmit() {
     console.log(this.loginForm);
+    this.onResetForm();
   }
 
   onAddHobbie() {
@@ -31,5 +44,31 @@ export class ReactiveComponent implements OnInit {
       new FormControl('', Validators.required)
     );
     console.log(this.loginForm.get('hobbies')['controls']);
+  }
+
+  forbiddenNamesValidator(control: FormControl): { [key: string]: boolean } {
+    if (this.forbiddenNames.indexOf(control.value) > -1) {
+      return { forbiddenName: true };
+    }
+    return null;
+  }
+
+  minLengthValidator(control: FormControl): { [key: string]: boolean } {
+    if (control.value && (control.value as string).length < 10) {
+      return { minLengthInvalid: true };
+    }
+    return null;
+  }
+
+  getErrorMessages(control: string): string[] {
+    let errorMessages: string[] = [];
+    for (const [key] of Object.entries(this.loginForm.get(control).errors)) {
+      errorMessages.push(this.errors[key]);
+    }
+    return errorMessages;
+  }
+
+  onResetForm(): void {
+    this.loginForm.reset();
   }
 }
