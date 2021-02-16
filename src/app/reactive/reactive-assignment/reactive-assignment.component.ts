52e07547d1a6415e6d3da7fb96c51aafdbb7b635
Joao Martins <martins.joao1909@gmail.com>
2021-02-16 06:03:49 -0300

    assignment...

--- src/app/reactive/reactive-assignment/reactive-assignment.component.ts
+++ src/app/reactive/reactive-assignment/reactive-assignment.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-assignment',
   templateUrl: './reactive-assignment.component.html',
   styleUrls: ['./reactive-assignment.component.css'],
 })
-export class ReactiveAssignmentComponent {
+export class ReactiveAssignmentComponent implements OnInit {
+  projectForm: FormGroup = new FormGroup({
+    userData: new FormGroup({
+      username: new FormControl('batata', Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+    }),
+    projStatus: new FormControl(null),
+  });
   projectStatus: string[] = ['Stable', 'Critical', 'Finished'];
+
+  ngOnInit() {
+    // this.configureForm();
+    this.projectForm = new FormGroup({
+      userData: new FormGroup({
+        username: new FormControl('batata', Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+      }),
+      projStatus: new FormControl('Stable'),
+    });
+  }
+
+  configureForm() {
+    this.projectForm = new FormGroup({
+      userData: new FormGroup({
+        username: new FormControl('batata', Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+      }),
+      projStatus: new FormControl(null),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.projectForm.value);
+  }
 }
